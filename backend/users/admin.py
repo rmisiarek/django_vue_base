73831ded3e6209d88e7b08556d2f89e209c584ff
Rafal <misiarek@gmail.com>
2019-03-12 19:53:55 +0100

    integration with djoser and djangorestframework

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import CustomUser
 
-# Register your models here.
+
+@admin.register(CustomUser)
+class CustomUserAdmin(admin.ModelAdmin):
+    pass
--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,6 @@
-from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class CustomUser(AbstractUser):
+    def __str__(self):
+        return f'{self.first_name} {self.last_name} ({self.username})'
