c52cd2fa488cc0f0ad19b62014fd3079f0e31a8e
Rafal <misiarek@gmail.com>
2019-03-26 20:27:34 +0100

    simplejwt as jwt backend

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -94,7 +94,8 @@ AUTH_PASSWORD_VALIDATORS = [
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework.authentication.TokenAuthentication',
+        # 'rest_framework.authentication.TokenAuthentication',
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
 }
 
@@ -102,6 +103,10 @@ DJOSER = {
     'SERIALIZERS': {
          'user_create': 'users.serializers.CustomUserRegistrationSerializer'
     }
+}
+
+SIMPLE_JWT = {
+   'AUTH_HEADER_TYPES': ('JWT',),
 }
 
 LANGUAGE_CODE = 'en-us'
--- backend/core/urls.py
+++ backend/core/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 
     path('api/auth/', include('djoser.urls')),
-    path('api/auth/', include('djoser.urls.authtoken')),
+    path('api/auth/', include('djoser.urls.jwt')),
+
 ]
