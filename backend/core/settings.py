fc54505f853dd60834e4a9a5a317edc2fbb552b2
Rafal <misiarek@gmail.com>
2019-10-26 18:36:17 +0200

    perms update & email backend changed

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -110,8 +110,8 @@ REST_FRAMEWORK = {
     ),
 }
 
-# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = get_env_variable('EMAIL_HOST')
 EMAIL_PORT = get_env_variable('EMAIL_PORT')
 EMAIL_USE_TLS = get_env_variable('EMAIL_USE_TLS')
--- backend/tasks/views.py
+++ backend/tasks/views.py
@@ -1,6 +1,6 @@
 from rest_framework import generics, views, filters
 from rest_framework.pagination import PageNumberPagination
-from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
 from . import mixins
@@ -18,7 +18,7 @@ class StandardResultsSetPagination(PageNumberPagination):
 class TaskCategoryList(generics.ListAPIView):
     queryset = models.TaskCategory.objects.all()
     serializer_class = serializers.TaskCategorySerializer
-    permission_classes = (AllowAny,)
+    permission_classes = (IsAuthenticated,)
 
 
 class TaskStatusAPIView(generics.ListAPIView,
@@ -27,7 +27,7 @@ class TaskStatusAPIView(generics.ListAPIView,
                         mixins.JwtUserInfoMixin):
 
     serializer_class = serializers.TaskStatusSerializer
-    permission_classes = (AllowAny,)
+    permission_classes = (IsAuthenticated,)
 
     def get_queryset(self):
         user_id = self.user_id()
@@ -39,7 +39,7 @@ class BaseTaskListDeleteAPIView(generics.ListAPIView,
                                 mixins.JwtUserInfoMixin):
 
     serializer_class = serializers.BaseTaskSerializer
-    permission_classes = (AllowAny,)
+    permission_classes = (IsAuthenticated,)
     pagination_class = StandardResultsSetPagination
     search_fields = ['title']
     filter_backends = (filters.SearchFilter,)
@@ -47,8 +47,7 @@ class BaseTaskListDeleteAPIView(generics.ListAPIView,
     def get_queryset(self):
         filter_by = self.request.GET.get("filter_by")
         filter_by_id = self.request.GET.get("id")
-        # user_id = self.user_id()
-        user_id = 4
+        user_id = self.user_id()
         if user_id:
             if filter_by == "priority":
                 return models.BaseTask.objects.filter(created_by=user_id, priority=filter_by_id)
@@ -67,12 +66,12 @@ class BaseTaskCreateUpdateAPIView(generics.CreateAPIView,
 
     queryset = models.BaseTask.objects.all()
     serializer_class = serializers.BaseTaskCreateUpdateSerializer
-    permission_classes = (AllowAny,)
+    permission_classes = (IsAuthenticated,)
 
 
 class BaseTaskBulkDelete(mixins.BaseTaskBulkActionMixin):
     serializer_class = serializers.BaseTaskSerializer
-    permission_classes = (AllowAny,)
+    permission_classes = (IsAuthenticated,)
 
     def perform_action(self, instance):
         instance.delete()
@@ -81,7 +80,7 @@ class BaseTaskBulkDelete(mixins.BaseTaskBulkActionMixin):
 
 class BaseTaskBulkComplete(mixins.BaseTaskBulkActionMixin):
     serializer_class = serializers.BaseTaskSerializer
-    permission_classes = (AllowAny,)
+    permission_classes = (IsAuthenticated,)
 
     def get_queryset(self):
         ids = self.request.data.get(self.lookup_field)
@@ -100,7 +99,7 @@ class BaseTaskBulkComplete(mixins.BaseTaskBulkActionMixin):
 
 class BaseTaskBulkStar(mixins.BaseTaskBulkActionMixin):
     serializer_class = serializers.BaseTaskSerializer
-    permission_classes = (AllowAny,)
+    permission_classes = (IsAuthenticated,)
 
     def get_queryset(self):
         ids = self.request.data.get(self.lookup_field)
@@ -124,7 +123,7 @@ class BaseStatsEisenhowerMatrix(views.APIView, mixins.JwtUserInfoMixin):
 
 
 class BaseStatsNewAndOld(views.APIView, mixins.JwtUserInfoMixin):
-    permission_classes = (AllowAny,)
+    permission_classes = (IsAuthenticated,)
 
     def get(self, request):
         statuses = stats.tasks_new_and_with_deadline_stats(how_much=3, user_id=self.user_id())
@@ -132,7 +131,7 @@ class BaseStatsNewAndOld(views.APIView, mixins.JwtUserInfoMixin):
 
 
 class BaseStatsStatuses(views.APIView, mixins.JwtUserInfoMixin):
-    permission_classes = (AllowAny,)
+    permission_classes = (IsAuthenticated,)
 
     def get(self, request):
         statuses = stats.tasks_statuses_stats(user_id=self.user_id())
@@ -140,7 +139,7 @@ class BaseStatsStatuses(views.APIView, mixins.JwtUserInfoMixin):
 
 
 class BaseStatsStatusesChart(views.APIView, mixins.JwtUserInfoMixin):
-    permission_classes = (AllowAny,)
+    permission_classes = (IsAuthenticated,)
 
     def get(self, request):
         data = stats.tasks_statuses_chart(user_id=self.user_id(), days=10)
