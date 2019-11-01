0a4e296e2ad74a0005730b57d85ffa79657af1a5
Rafal <misiarek@gmail.com>
2019-11-01 22:28:35 +0100

    literals fix

--- frontend/src/components/tasks/literals.js
+++ frontend/src/components/tasks/literals.js
@@ -1,6 +1,6 @@
 export const TASKS_PRIORITY_LIST = [
-  {'value': '1', 'text': 'Urgent and important'},
-  {'value': '2', 'text': 'Important and not urgent'},
-  {'value': '3', 'text': 'Urgent and not important'},
-  {'value': '4', 'text': 'Not important and not urgent'},
-]
+  { value: '1', text: 'Urgent and important' },
+  { value: '2', text: 'Important and not urgent' },
+  { value: '3', text: 'Urgent and not important' },
+  { value: '4', text: 'Not important and not urgent' },
+];
